522b96bfdddaedaea1d9ec9b4384cfddaefd0b64
ElasticBottle <winstonyeo99@yahoo.com>
2025-04-15 11:52:34 +0200

    build: add sst deploy to aws

--- apps/backend/src/routes/api/index.ts
+++ apps/backend/src/routes/api/index.ts
@@ -1,5 +1,6 @@
+import type { HonoEnv } from "@/lib/hono";
 import { Hono } from "hono";
 
-export const apiRouter = new Hono().get("/", (c) =>
-  c.json({ message: "Hello, World from the backend!" }),
-);
+export const apiRouter = new Hono<HonoEnv>().get("/", (c) => {
+  return c.json({ message: "Hello, World from the backend!" });
+});
