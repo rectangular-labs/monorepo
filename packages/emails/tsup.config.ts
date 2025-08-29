95140d0f42a16e57828b5d0cd35138056e253d6d
ElasticBottle <winstonyeo99@yahoo.com>
2025-08-29 13:52:34 +0200

    build(emails): configure build

--- packages/emails/tsup.config.ts
+++ packages/emails/tsup.config.ts
@@ -4,12 +4,12 @@ export default defineConfig((options) => {
   const isWatch = options.watch;
   return {
     ...options,
-    entry: ["src/index.ts"],
+    entry: ["src/index.ts", "src/drivers/**/*.ts"],
     format: ["esm" as const],
     splitting: true, //technically true by default for esm, but we'll be explicit
     sourcemap: !isWatch,
     minify: !isWatch,
     clean: !isWatch,
-    onSuccess: "tsc",
+    onSuccess: "tsc -p tsconfig.build.json",
   };
 });
