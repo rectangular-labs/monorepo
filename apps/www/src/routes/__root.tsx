f52638458d733ceefd7740789c1912a36ffcdeaa
ElasticBottle <winstonyeo99@yahoo.com>
2025-09-09 02:08:15 +0200

    test: custom theme provider

--- apps/www/src/routes/__root.tsx
+++ apps/www/src/routes/__root.tsx
@@ -99,7 +99,7 @@ function RootLayout() {
           defaultTheme="system"
           enableSystem
           // cloudflare magic see https://github.com/pacocoursey/next-themes?tab=readme-ov-file#using-with-cloudflare-rocket-loader
-          scriptProps={{ "data-cfasync": "false" }}
+          // scriptProps={{ "data-cfasync": "false" }}
         >
           <Outlet />
           <Toaster />
