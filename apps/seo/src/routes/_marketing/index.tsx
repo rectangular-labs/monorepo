13a59150b64f1144a5b9014a703346b3829cef8d
Winston Yeo <44563205+ElasticBottle@users.noreply.github.com>
2025-10-11 07:57:15 +0800

    add apollo script (#307)

--- apps/seo/src/routes/_marketing/index.tsx
+++ apps/seo/src/routes/_marketing/index.tsx
@@ -8,7 +8,23 @@ import { LogoCloud } from "./-components/logo-cloud";
 import Pricing from "./-components/pricing";
 import { Stats } from "./-components/stats";
 
-export const Route = createFileRoute("/_marketing/")({ component: App });
+export const Route = createFileRoute("/_marketing/")({
+  component: App,
+
+  head: () => ({
+    scripts: [
+      {
+        src:
+          "https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache=" +
+          Math.random().toString(36).substring(7),
+        async: true,
+        defer: true,
+        onload:
+          "window.trackingFunctions.onLoad({appId:'68e8553db8cc65001148717d'})",
+      },
+    ],
+  }),
+});
 
 function App() {
   return (
