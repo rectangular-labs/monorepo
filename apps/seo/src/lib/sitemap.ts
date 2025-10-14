108ac3986ec129a64118535cab7eb9dc85ee3f76
ElasticBottle <winstonyeo99@yahoo.com>
2025-10-14 03:34:30 -0700

    fix: sitemap generation

--- apps/seo/src/lib/sitemap.ts
+++ apps/seo/src/lib/sitemap.ts
@@ -2,6 +2,9 @@ import type { Sitemap } from "tanstack-router-sitemap";
 import type { FileRouteTypes } from "~/routeTree.gen";
 export type TRoutes = FileRouteTypes["fullPaths"];
 
+import type { seoBlogSource as blogSource } from "@rectangular-labs/content";
+import { createJiti } from "jiti";
+
 export const sitemap: Sitemap<TRoutes> = {
   siteUrl: "https://www.fluidposts.com",
   defaultPriority: 0.5,
@@ -16,7 +19,13 @@ export const sitemap: Sitemap<TRoutes> = {
     },
     // Dynamic route example
     "/blog/$": async () => {
-      const { seoBlogSource } = await import("@rectangular-labs/content");
+      const jiti = createJiti(import.meta.url);
+      const { seoBlogSource } = (await jiti.import(
+        "@rectangular-labs/content",
+      )) as {
+        seoBlogSource: typeof blogSource;
+      };
+
       return seoBlogSource.generateParams().map((params) => ({
         path: `/blog/${params.slug}`,
         priority: 0.8,
