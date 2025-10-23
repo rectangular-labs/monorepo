b4320d03d1802fe6088f3a4b13cf5cec7ad43814
ElasticBottle <winstonyeo99@yahoo.com>
2025-10-23 16:27:59 +0800

    feat: add ai elements

--- packages/ui/src/components/ui/carousel.tsx
+++ packages/ui/src/components/ui/carousel.tsx
@@ -117,6 +117,7 @@ function Carousel({
         canScrollNext,
       }}
     >
+      {/** biome-ignore lint/a11y/useSemanticElements: from shadCN */}
       <div
         aria-roledescription="carousel"
         className={cn("relative", className)}
@@ -156,6 +157,7 @@ function CarouselItem({ className, ...props }: React.ComponentProps<"div">) {
   const { orientation } = useCarousel();
 
   return (
+    // biome-ignore lint/a11y/useSemanticElements: from shadCN
     <div
       aria-roledescription="slide"
       className={cn(
