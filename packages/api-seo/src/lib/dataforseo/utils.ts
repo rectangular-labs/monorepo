35bc46d8d4d1ac8d28e947b1e0cdb1b915e2f7d1
ElasticBottle <winstonyeo99@yahoo.com>
2026-01-02 08:41:18 +0800

    feat(api-seo): add cahching support for dataforseo function calls

--- packages/api-seo/src/lib/dataforseo/utils.ts
+++ packages/api-seo/src/lib/dataforseo/utils.ts
@@ -3,6 +3,7 @@ import {
   COUNTRY_CODE_MAP,
 } from "@rectangular-labs/core/schemas/project-parsers";
 import { client } from "@rectangular-labs/dataforseo/client";
+import { ok, type Result, safe } from "@rectangular-labs/result";
 import { apiEnv } from "../../env";
 
 export function getLocationAndLanguage(project: {
@@ -29,4 +30,45 @@ export function configureDataForSeoClient() {
     auth: () =>
       `${apiEnv().DATAFORSEO_USERNAME}:${apiEnv().DATAFORSEO_PASSWORD}`,
   });
+}
+
+export function getSerpCacheKey(
+  primaryKeyword: string,
+  locationName: string,
+  languageCode: string,
+) {
+  return `serp-${primaryKeyword}-${locationName}-${languageCode}`;
+}
+
+export async function fetchWithCache<T>({
+  key,
+  fn,
+  cacheKV,
+  options,
+}: {
+  key: string;
+  fn: () => Promise<T>;
+  cacheKV: KVNamespace;
+  options?: {
+    ttlSeconds?: number;
+    waitUntil?: (promise: Promise<void>) => void;
+  };
+}): Promise<Result<T, Error>> {
+  const cachedResults = await safe(() => {
+    return cacheKV.get<T>(key, "json");
+  });
+  if (cachedResults.ok && !!cachedResults.value) {
+    return ok(cachedResults.value);
+  }
+
+  const result = await safe(fn);
+  if (!result.ok) return result;
+  const putResult = await safe(() => {
+    return cacheKV.put(key, JSON.stringify(result.value), {
+      expirationTtl: options?.ttlSeconds ?? 60 * 60, // default to 1 hour
+    });
+  });
+  if (!putResult.ok) return putResult;
+
+  return ok(result.value);
 }
